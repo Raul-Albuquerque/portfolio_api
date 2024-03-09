da4ef4a5c20f4471c127fdd88fddadc5176645e7
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-03-09 01:13:44 -0300

    test

--- api/app.py
+++ api/app.py
@@ -2,30 +2,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-from mock_data.about import about
-
-livros = [
-  {
-    'id': 1,
-    'titulo': 'O Senhor dos Aneis -  A Sociedade do Anel',
-    'autor': 'J.R.R. Tolkien'
-  },
-  {
-    'id': 2,
-    'titulo': 'Harry Potter e a Pedra Filosofal',
-    'autor': 'J.K. Howling'
-  },
-  {
-    'id': 3,
-    'titulo': 'Habitos Atomicos',
-    'autor': 'James Clear'
-  },
-]
-
-# Consultar(todos)
-@app.route('/livros', methods=['GET'])
-def obter_livros():
-    return jsonify(livros)
+from api.mock_data.about import about
 
 @app.route('/about', methods=['GET'])
 def get_about():
