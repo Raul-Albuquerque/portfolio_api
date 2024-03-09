936ceffda17bdde940d00c657681280223be4543
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-03-09 01:20:56 -0300

    feat: portfolio information

--- api/app.py
+++ api/app.py
@@ -3,10 +3,25 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 from api.mock_data.about import about
+from api.mock_data.contacts import contacts
+from api.mock_data.courses import courses
+from api.mock_data.projects import projects
 
 @app.route('/about', methods=['GET'])
 def get_about():
     return jsonify(about)
+
+@app.route('/contacts', methods=['GET'])
+def get_contacts():
+    return jsonify(contacts)
+
+@app.route('/courses', methods=['GET'])
+def get_courses():
+    return jsonify(courses)
+
+@app.route('/projects', methods=['GET'])
+def get_projects():
+    return jsonify(projects)
 
 
 if __name__ == '__main__':
