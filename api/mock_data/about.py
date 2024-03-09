bf3fe86a096020d1f59482649f89070ed70b3083
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-03-09 02:02:53 -0300

    fix: response bug

--- api/mock_data/about.py
+++ api/mock_data/about.py
@@ -3,7 +3,7 @@
 about = {
   "fullname": "RaulAlbuquerque",
   "name": "raul",
-  "aboutMe": "Sou um desenvolvedor Front-End com um perfil criativo, extremamente interessado em tecnologia e pela capacidade de promover experiências através de uma tela. Fui Oficial do Exército durante 7 (sete) anos, onde pude desenvolver minha disciplina, resiliência, responsabilidade, liderança, colaboratividade, equilíbrio emocional, flexibilidade e sempre busquei solucionar os problemas da melhor forma. Nesse período, tive contato com pessoas de diferentes locais do país, fortalecendo minha empatia, comunicação e capacidade de trabalhar em equipe.",
+  "aboutMe": u"Sou um desenvolvedor Front-End com um perfil criativo, extremamente interessado em tecnologia e pela capacidade de promover experiências através de uma tela. Fui Oficial do Exército durante 7 (sete) anos, onde pude desenvolver minha disciplina, resiliência, responsabilidade, liderança, colaboratividade, equilíbrio emocional, flexibilidade e sempre busquei solucionar os problemas da melhor forma. Nesse período, tive contato com pessoas de diferentes locais do país, fortalecendo minha empatia, comunicação e capacidade de trabalhar em equipe.",
   "hardskills": [
     "html",
     "css",
@@ -30,13 +30,13 @@ about = {
     "figma",
   ],
   "softskills": [
-    "equilíbrio emocional",
+    u"equilíbrio emocional",
     "colaboratividade",
     "empatia",
     "flexibilidade",
-    "liderança",
+    u"liderança",
     "responsabilidade",
-    "resiliência",
+    u"resiliência",
     "comunicação clara",
     "disciplina",
     "capacidade de trabalhar em equipe"
